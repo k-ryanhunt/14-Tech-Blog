3a2d145ba2311caa8b80e551885ea8891e8385d3
K Ryan <hunt.kelsearyan@gmail.com>
2021-05-15 22:27:26 -0600

    I think the file structure is set up, some of it is working but some of the buttons do not

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const { User, Blog } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const blogs = await Blog.bulkCreate(blogData, {
+      individualHooks: true,
+      returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
